925dfacf1ea6880eefa689e83f7113f651000c05
Zhongyi Fu <zyfu90@gmail.com>
2024-07-08 19:20:52 -0600

    add current position

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ function App() {
   return (
     <div>
       <Header />
+      <LineItem name="Edmonton, AB" content="DevOps Engineer at Bazaarvoice" icon="work" />
+      <LineItem name="Email" content="zhongyi.fu@bazaarvoice.com" icon="email" isEmail />
       <LineItem name="Mobile" content="8252885204" icon="local_phone" isPhone />
-      <LineItem name="Email" content="zyfu90@gmail.com" icon="email" isEmail />
-      <LineItem name="Bow Valley College" content="Cloud Computing" icon="school" />
       <LineItem
         name="Macquarie University"
         content="Master of Internetworking and Cybersecurity"
         icon="school"
       />
+      <LineItem name="Bow Valley College" content="Cloud Computing" icon="school" />
     </div>
   );
 }
